9a98e0b925a386d5d4a14ddb89215b0e9c97ea57
Jamal Moir <jamal.moir@gmail.com>
2017-02-13 16:04:00 +0000

    Add Neural Network screen with no functionality

--- machine_learning_demo.py
+++ machine_learning_demo.py
@@ -1,16 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
 
+Config.set('graphics', 'width', '1200')
+Config.set('graphics', 'height', '600')
+Config.set('graphics', 'resizable', False)
 
 Builder.load_file('mlgui.kv')
+
 
 class MainScreen(Screen):
     pass
 
+
+class NeuralNetworkScreen(Screen):
+    pass
+
 screen_manager = ScreenManager()
 
-screen_manager.add_widget(MainScreen(name='main'))
+screen_manager.add_widget(MainScreen(name='main_screen'))
+screen_manager.add_widget(NeuralNetworkScreen(name='nn_screen'))
 
 
 class MLGUIApp(App):
