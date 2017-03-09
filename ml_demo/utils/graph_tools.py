5e0aad93757a97e7ea09d792f3eba820c77a8b9f
Jamal Moir <jamal.moir@gmail.com>
2017-03-09 14:53:31 +0000

    Improve decision tree drawing

--- ml_demo/utils/graph_tools.py
+++ ml_demo/utils/graph_tools.py
@@ -20,11 +20,11 @@ class TreeTraverse(object):
             self.edges.append(Edge(a=parent, b=node_num))
 
         if model[0] == -1:
-            self.nodes.append(Node(num=node_num, attribute=model[0], value=value, output=model[1]))
+            self.nodes.append(Node(num=node_num, attribute=None, value=value, output=str(model[1])))
             return
         else:
             self.nodes.append(Node(num=node_num, attribute=model[0], value=value, output=None))
             for i, sub_model in enumerate(model[1:]):
-                self.get_nodes_and_edges(model=sub_model, attribute=sub_model[0], value=i, parent=node_num, layer=layer+1)
+                self.get_nodes_and_edges(model=sub_model, attribute=str(sub_model[0]), value=i, parent=node_num, layer=layer+1)
 
         return self.nodes, self.edges
